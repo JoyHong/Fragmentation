eb17bbd5fa23856f6af8b0b4026109200e9597d9
fuxingguo <1978640054@qq.com>
2025-03-24 14:02:55 +0800

    bug:尝试规避addMockView方法中fromView移除后仍有parent引起的crash

--- fragmentation_core/src/main/java/me/yokeyword/fragmentation/TransactionDelegate.java
+++ fragmentation_core/src/main/java/me/yokeyword/fragmentation/TransactionDelegate.java
@@ -574,8 +574,18 @@ class TransactionDelegate {
             }
         };
 
-        mock.addView(fromView);
-        container.addView(mock);
+        ViewGroup parent = (ViewGroup) fromView.getParent();
+        if (parent != null) {
+            parent.removeView(fromView);
+        }
+        container.post(() -> {
+            if (fromView.getParent() == null) {
+                mock.addView(fromView);
+                container.addView(mock);
+            } else {
+                Log.e(TAG, "fromView still has parent after removeView(), skipping add to prevent crash.");
+            }
+        });
         return mock;
     }
 
